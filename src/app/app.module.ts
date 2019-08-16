f8abca602051b146051eb5a20fef4bd1b2067989
washucode <estherw8525@gmail.com>
2019-08-16 15:32:50 +0300

    add initial structure of webste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QoutesFormComponent } from './qoutes-form/qoutes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    QoutesFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/qoutes-form/qoutes-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qoutes-form',
+  templateUrl: './qoutes-form.component.html',
+  styleUrls: ['./qoutes-form.component.css']
+})
+export class QoutesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
