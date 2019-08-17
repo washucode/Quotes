67d0f6d9ae9772846059333da2cccb441261238c
washucode <estherw8525@gmail.com>
2019-08-17 16:25:33 +0300

    add an item showDateName which is a boolean in list Quotes and set it to false

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
-
+   showDateName : boolean;
   constructor(  public upVote:number, public downVote:number,public id:number, public author:string, public name : string,public details:string){
-
+    this.showDateName = false;
   }
 
 
