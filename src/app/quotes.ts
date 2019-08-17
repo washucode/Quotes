f8a9b26a9b861c33ebf362f868f71c7619b16eb7
washucode <estherw8525@gmail.com>
2019-08-17 11:18:29 +0300

    add upvote and downvote to class Qoutes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
 
-  constructor(  public id:number, public author:string, public name : string,public details:string){
+  constructor(  public upVote:number, public downVote:number,public id:number, public author:string, public name : string,public details:string){
 
   }
 
