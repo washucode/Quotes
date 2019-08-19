c4445a06ef39810d8b0e11a2fbff4ec4bf6edc96
washucode <estherw8525@gmail.com>
2019-08-19 04:21:09 +0300

    create pipe to show how long ago the quote was uploaded

--- src/app/qoute-item/qoute-item.component.ts
+++ src/app/qoute-item/qoute-item.component.ts
@@ -10,7 +10,7 @@ export class QouteItemComponent implements OnInit {
 
   quotes: Quotes[] = [
     new Quotes(0,0,1, 'Audre Lorde','Wachuka','When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',new Date(2019,1,12)),
-    new Quotes(0,0,2, 'Oscar Wilde','Wachuka','To live is the rarest thing in the world. Most people exist, that is all',new Date(2019,1,12)),
+    new Quotes(0,0,2, 'Oscar Wilde','Wachuka','To live is the rarest thing in the world. Most people exist, that is all',new Date(2019,8,7)),
     new Quotes(0,0,2, 'Andre Gide, Autumn Leaves','Wachuka','It is better to be hated for what you are than to be loved for what you are not.',new Date(2019,1,12)),
 
   ];
@@ -56,7 +56,7 @@ export class QouteItemComponent implements OnInit {
   }
 
 
-  
+
 
 
   constructor() {
--- src/app/time-ago.pipe.ts
+++ src/app/time-ago.pipe.ts
@@ -5,8 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeAgoPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): any {
+    let now:Date = new Date();
+    let nowTime:any = new Date(now.getTime());
+    let valueTime:any = new Date(value.getTime());
+    var timeDifference = Math.abs(nowTime - valueTime);
+    const secondInHour = 3600000;
+    var timeDifferenceFinal = timeDifference/secondInHour;
+
+
+     if(timeDifferenceFinal >=168){
+       return   Math.round(timeDifferenceFinal/168)  + "days";
+     }
+     else if(timeDifferenceFinal >= 48){
+       return   Math.round(timeDifferenceFinal/24) + "days";
+     }
+    else if (timeDifferenceFinal >= 1 ){
+      return Math.round(timeDifferenceFinal) + "hours";
+
+    }else if (0.02 <= timeDifferenceFinal && timeDifferenceFinal<= 1){
+      return Math.round(timeDifferenceFinal * 60)+ "mins";
+    }else if (timeDifferenceFinal<0.02){
+      return Math.round(timeDifferenceFinal * 3600) +"secs";
+    }
+
   }
 
 }
